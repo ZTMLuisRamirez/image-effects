d5884a804c771c6a1b3d14fc9e72864a5a59d6ce
Luis R <jaskokoyn@gmail.com>
2024-07-18 19:37:36 -0400

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const htmlPlugin = require("html-webpack-plugin");
+const wasmPlugin = require("@wasm-tool/wasm-pack-plugin");
+const { experiments } = require("webpack");
+
+module.exports = {
+  entry: "./public/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
+  plugins: [
+    new htmlPlugin({
+      template: "./public/index.html",
+    }),
+    new wasmPlugin({
+      crateDirectory: path.resolve(__dirname, "."),
+    }),
+  ],
+  experiments: {
+    asyncWebAssembly: true,
+  },
+};
